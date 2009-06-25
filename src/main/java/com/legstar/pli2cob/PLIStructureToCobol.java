7e802d840759b57b58146aa4ca8449730f6d12ef
fady.moussallam <fady.moussallam@gmail.com>
2009-06-25 11:07:26 +0000

    Added COBOL generation for arrays and structures.

--- src/main/java/com/legstar/pli2cob/PLIStructureToCobol.java
+++ src/main/java/com/legstar/pli2cob/PLIStructureToCobol.java
@@ -3,13 +3,14 @@ package com.legstar.pli2cob;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.antlr.runtime.tree.CommonTree;
 import org.antlr.runtime.tree.CommonTreeAdaptor;
 import org.antlr.runtime.tree.TreeAdaptor;
 import org.antlr.stringtemplate.StringTemplate;
 
-import com.legstar.pli2cob.PLIDataItem.Base;
-import com.legstar.pli2cob.PLIDataItem.Scale;
+import com.legstar.pli2cob.model.PLIDataDimension;
+import com.legstar.pli2cob.model.PLIDataItem;
+import com.legstar.pli2cob.model.PLIDataItem.Base;
+import com.legstar.pli2cob.model.PLIDataItem.Scale;
 
 /**
  * Produce COBOL fragments from PLI structures after they are parsed as
@@ -19,21 +20,29 @@ import com.legstar.pli2cob.PLIDataItem.Scale;
 public class PLIStructureToCobol {
 
 	/** Main template for a COBOL fragment. */
-	public static StringTemplate _treeST =
+	public static StringTemplate _cobolFragmentST =
 		new StringTemplate(
 				"      *\n" +
 				"      *\n" +
 				"      *\n" +
-				"$nodes$" +
+				"$dataDescriptions$" +
 		"\n");
 
-	/** Item template. */
-	public static StringTemplate _itemST =
-		new StringTemplate("       $level$  $name$$attributes$.\n");
+	/** COBOL data description template. */
+	public static StringTemplate _dataDescriptionST =
+		new StringTemplate("$level$ $name$$attributes$.\n");
 
 	/** Attributes template. */
 	public static StringTemplate _attributesST =
 		new StringTemplate("$occurs$$pictureUsage$");
+
+	/** Occurs template. */
+	public static StringTemplate _occursST =
+		new StringTemplate(" OCCURS $maxOccurs$$dependingOn$");
+
+	/** Depending on template. */
+	public static StringTemplate _dependingOnST =
+		new StringTemplate(" DEPENDING $dependingOn$");
 
 	/** Picture template for picture strings. */
 	public static StringTemplate _pictureValueST =
@@ -46,8 +55,8 @@ public class PLIStructureToCobol {
 	/** Picture template for varying character. */
 	public static StringTemplate _characterVaryingPictureValueST =
 		new StringTemplate(".\n"
-				+ "            $_subLevel$ LEN PIC 9(4) BINARY.\n"
-				+ "            $_subLevel$ CHAR PIC X OCCURS 1 TO $_length$ DEPENDING ON LEN");
+				+ "$_subLevel$ LEN PIC 9(4) BINARY.\n"
+				+ "$_subLevel$ CHAR PIC X OCCURS 1 TO $_length$ DEPENDING ON LEN");
 
 	/** Picture template for graphic. */
 	public static StringTemplate _graphicPictureValueST =
@@ -84,6 +93,10 @@ public class PLIStructureToCobol {
 	/** Detects repetition factors in PLI picture.*/
 	private static final Pattern REPETITION_FACTOR_PATTERN =
 		Pattern.compile("\\(\\d+\\)");
+	
+	/** Used to create indentation on data descriptions.*/
+	private static final String WHITESPACES = "                                        ";
+
 	/**
 	 * Converts one or multiple PLI declare statements to COBOL data description clauses.
 	 * @param ast the abstract syntax tree
@@ -91,35 +104,45 @@ public class PLIStructureToCobol {
 	 * @throws CobolFormatException if conversion fails
 	 */
 	public String convert(
-			final CommonTree ast) throws CobolFormatException {
+			final Object ast) throws CobolFormatException {
 		TreeAdaptor adaptor = new CommonTreeAdaptor();
-		StringTemplate treeST = _treeST.getInstanceOf();
-		if (ast.getType() == PLIStructureParser.DATA_ITEM) {
-			treeST.setAttribute("nodes", getItemST( adaptor, ast));
-		} else {
-			int n = adaptor.getChildCount(ast);
-			for (int i = 0; i < n; i++) {
-				Object astItem = adaptor.getChild(ast, i);
-				int type = adaptor.getType(astItem);
-				if (type == PLIStructureParser.DATA_ITEM) {
-					treeST.setAttribute("nodes", getItemST( adaptor, astItem));
-				}
-			}
+		StringTemplate cobolFragmentST = _cobolFragmentST.getInstanceOf();
+		setDataDescription(adaptor, cobolFragmentST, ast);
+		return cobolFragmentST.toString();
+	}
+	
+	/**
+	 * Recursively traverses the abstract syntax tree generating data descriptions for each node.
+	 * @param adaptor the antlr tree adaptor in use
+	 * @param cobolFragmentST the main string template that contains the data descriptions produced
+	 * @param astItem the abstract syntax subtree
+	 * @throws CobolFormatException if formatting fails
+	 */
+	public void setDataDescription(
+			final TreeAdaptor adaptor,
+			final StringTemplate cobolFragmentST,
+			final Object astItem) throws CobolFormatException {
+		if (adaptor.getType(astItem) == PLIStructureParser.DATA_ITEM) {
+			cobolFragmentST.setAttribute("dataDescriptions",
+					getDataDescriptionST( adaptor, astItem));
 		}
-		return treeST.toString();
+		int n = adaptor.getChildCount(astItem);
+		for (int i = 0; i < n; i++) {
+			setDataDescription(adaptor, cobolFragmentST, adaptor.getChild(astItem, i));
+		}
 	}
 
 	/**
-	 * Produce an item definition.
+	 * Produce a COBOL data description for a node.
 	 * @param adaptor the antlr tree adaptor in use
-	 * @param astItem the abstract syntax subtree
+	 * @param astItem the abstract syntax node
 	 * @return an antlr string template
 	 * @throws CobolFormatException if formatting fails
 	 */
-	public StringTemplate getItemST(
+	public StringTemplate getDataDescriptionST(
 			final TreeAdaptor adaptor, final Object astItem) throws CobolFormatException {
 		PLIDataItem dataItem = new PLIDataItem(adaptor, astItem);
-		StringTemplate nodeST = _itemST.getInstanceOf();
+		StringTemplate nodeST = _dataDescriptionST.getInstanceOf();
 		nodeST.setAttribute("level", formatLevel(dataItem.getLevel()));
 		nodeST.setAttribute("name", formatName(dataItem.getName()));
 		nodeST.setAttribute("attributes", getAttributesST(adaptor, dataItem));
@@ -137,12 +160,19 @@ public class PLIStructureToCobol {
 			final TreeAdaptor adaptor, final PLIDataItem dataItem) throws CobolFormatException {
 		StringTemplate nodeST = _attributesST.getInstanceOf();
 		nodeST.setAttribute("pictureUsage", formatPictureAndUsage(dataItem));
+		nodeST.setAttribute("occurs", formatOccurs(dataItem));
 		return nodeST;
 	}
 
 	/**
 	 * COBOL level numbers are in the 1-49 range. It is customary to format them
 	 * as 2 digits.
+	 * <p/>
+	 * This adds white spaces to the left of the level number using these simple rules:
+	 * <ul>
+	 * <li>Level 1 is placed at position 8 (start of area A)</li>
+	 * <li>Levels higher than 1 are placed at position 12 (start of area B)</li>
+	 * </ul>
 	 * @param level the proposed level
 	 * @return a formatted level
 	 * @throws CobolFormatException if level is invalid for COBOL
@@ -152,7 +182,8 @@ public class PLIStructureToCobol {
 		if (level < 1 || level > 49) {
 			throw new CobolFormatException("Level " + level + " is invalid for COBOL");
 		}
-		return String.format("%1$02d", level);
+		String indentedLevel = (level == 1) ? WHITESPACES.substring(0, 7) : WHITESPACES.substring(0, 11);
+		return indentedLevel + String.format("%1$02d", level);
 	}
 
 	/**
@@ -189,8 +220,77 @@ public class PLIStructureToCobol {
 	}
 
 	/**
+	 * Produce a COBOL occurs clause for array items.
+	 * <p/>
+	 * COBOL does not support lower bounds in the PLI sense. If a lower bound
+	 * is present, we assume it reduces the array capacity.
+	 * COBOL has a notion of minimum capacity (as in OCCURS n TO ...) but there
+	 * is no equivalent in PLI.
+	 * <p/>
+	 * In PLI both lower and upper bound can be variable. Here we support variability
+	 * only if there are no lower bounds.
+	 * <p/>
+	 * Multiple dimension arrays are supported with severe restrictions in this version:
+	 * <ul>
+	 * <li>Dimensions must not have a lower bound</li>
+	 * <li>Dimensions must not be variable</li>
+	 * </ul>
+	 * @param dataItem data item model
+	 * @return an occurs clause or null if not an array
+	 * @throws CobolFormatException if formatting fails
+	 */
+	public StringTemplate formatOccurs(
+			final PLIDataItem dataItem) throws CobolFormatException {
+		StringTemplate nodeST = null;
+		if (dataItem.getDimensions().size() == 0) {
+			return null;
+		}
+		if (dataItem.getDimensions().size() == 1) {
+			PLIDataDimension dimension = dataItem.getDimensions().get(0);
+			int maxOccurs = dimension.getHbound().getBound();
+			if (dimension.getLbound() != null) {
+				maxOccurs += 1 - dimension.getLbound().getBound();
+				if (dimension.getLbound().getRefer() != null) {
+					throw new CobolFormatException(
+							"Unsupported variable lower bound: " + dataItem.toString());
+				}
+			}
+			nodeST = _occursST.getInstanceOf();
+			nodeST.setAttribute("maxOccurs", maxOccurs);
+			if (dimension.getHbound().getRefer() != null) {
+				/* Make sure there is no lower bound */
+				if (dimension.getLbound() != null &&  dimension.getLbound().getBound() > 1) {
+					throw new CobolFormatException(
+							"Unsupported variable upper bound: " + dataItem.toString());
+				}
+				StringTemplate dependingOnST = _dependingOnST.getInstanceOf();
+				dependingOnST.setAttribute("dependingOn", dimension.getHbound().getRefer());
+				nodeST.setAttribute("dependingOn", dependingOnST);
+			}
+			
+		}
+		if (dataItem.getDimensions().size() > 1) {
+			nodeST = _occursST.getInstanceOf();
+			int maxOccurs = 1;
+			for (PLIDataDimension dimension : dataItem.getDimensions()) {
+				if (dimension.getLbound() != null &&  dimension.getLbound().getBound() > 1) {
+					throw new CobolFormatException(
+							"Unsupported lower bound for multi-dimension array: " + dataItem.toString());
+				}
+				if (dimension.getHbound().getRefer() != null) {
+					throw new CobolFormatException(
+							"Unsupported variable higher bound for multi-dimension array: " + dataItem.toString());
+				}
+				maxOccurs *= dimension.getHbound().getBound();
+			}
+			nodeST.setAttribute("maxOccurs", maxOccurs);
+		}
+		return nodeST;
+	}
+
+	/**
 	 * Produce a COBOL picture and picture clause for elementary items.
-	 * @param length string length
+	 * @param dataItem data item model
 	 * @return a picture clause
 	 * @throws CobolFormatException if formatting fails
 	 */
@@ -318,12 +418,28 @@ public class PLIStructureToCobol {
 	 * COBOL pictures are close to PLI pictures. There are differences though:
 	 * <ul>
 	 * <li>Repetition factors are inverted: (n)9 --> 9(n)</li>
+	 * <li>T signals overpunch which is implicit in COBOL</li>
 	 * </ul>
 	 * TODO We need to handle overpunch
-	 * @param picture
-	 * @return
+	 * @param picture the PLI picture
+	 * @return the COBOL picture
 	 */
 	public String cobolPictureFromPLI(final String picture) {
+		if (picture == null || picture.length() == 0) {
+			return picture;
+		}
+		String cobolPicture = invertRepetitionFactor(picture);
+		cobolPicture = resolveOverpunch(cobolPicture);
+		return cobolPicture;
+	}
+	
+	/**
+	 * Inverts the repetition factors.
+	 * @param picture the PLI picture
+	 * @return a string with repetition factors inverted
+	 * TODO scaling factors are missed for repetition factors
+	 */
+	public String invertRepetitionFactor(final String picture) {
 		StringBuilder sb = new StringBuilder();
 		int current = 0;
 		Matcher matcher = REPETITION_FACTOR_PATTERN.matcher(picture);
@@ -337,6 +453,29 @@ public class PLIStructureToCobol {
 			sb.append(picture.substring(current)) ;
 		}
 		return sb.toString();
+	}
+	
+	/**
+	 * In PLI any character can be overpunched. In COBOL, only the first
+	 * or last. Here we oversimplify by assuming the PLI picture has only one
+	 * T as the first or last position.
+	 * @param picture the PLI picture
+	 * @return a picture with overpunch replaced
+	 */
+	public String resolveOverpunch(final String picture) {
+		if (picture.charAt(0) == 'T') {
+			return '9' + picture.substring(1) + " LEADING";
+		}
+		if (picture.charAt(0) == 'S') {
+			return picture + " SIGN SEPARATE LEADING";
+		}
+		if (picture.charAt(picture.length() - 1) == 'T') {
+			return  picture.substring(0, picture.length() - 1) + '9';
+		}
+		if (picture.charAt(picture.length() - 1) == 'S') {
+			return  'S' + picture.substring(0, picture.length() - 1) + " SIGN SEPARATE";
+		}
+		return picture;
 	}
 	
 }
---
+++ src/main/java/com/legstar/pli2cob/model/AbstractPLIData.java
@@ -0,0 +1,58 @@
+package com.legstar.pli2cob.model;
+
+import org.antlr.runtime.tree.TreeAdaptor;
+
+
+/**
+ * Common model for classes that can be built from an abstract syntax tree node.
+ *
+ */
+public abstract class AbstractPLIData {
+
+	/**
+	 * For single valued attributes, this returns either the value or a default
+	 * one if none is found.
+	 * @param adaptor the tree navigator
+	 * @param astItem the data item abstract syntax subtree
+	 * @param attributeType the type of attribute
+	 * @param defaultValue the default value
+	 * @return the attribute value or a default. Null if the element is nil.
+	 */
+	public static Object getAttributeValue(
+			final TreeAdaptor adaptor,
+			final Object astItem,
+			final int attributeType,
+			final Object defaultValue) {
+		if (adaptor.isNil(astItem)) {
+			return null;
+		}
+		Object value = getAttribute(adaptor, astItem, attributeType);
+		if (value == null) {
+			return defaultValue;
+		} else {
+			return adaptor.getText(adaptor.getChild(value, 0));
+		}
+	}
+
+	/**
+	 * Search a tree direct childs for an attribute type.
+	 * @param adaptor the tree navigator
+	 * @param astItem the data item abstract syntax subtree
+	 * @param attributeType the type of attribute
+	 * @return the attribute tree item if found, null otherwise.
+	 */
+	public static Object getAttribute(
+			final TreeAdaptor adaptor,
+			final Object astItem,
+			final int attributeType) {
+		int n = adaptor.getChildCount(astItem);
+		for (int i = 0; i < n; i++) {
+			Object attribute = adaptor.getChild(astItem, i);
+			if (adaptor.getType(attribute) == attributeType) {
+				return attribute;
+			}
+		}
+		return null;
+	}
+
+}
